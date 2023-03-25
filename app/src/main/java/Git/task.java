6e6b57a5894bd455bee495af41c28b7b7fdbb889
Ehsanul-karim <60375625+Ehsanul-karim@users.noreply.github.com>
2023-03-25 20:08:56 +0600

    task java class added from master

---
+++ app/src/main/java/Git/task.java
@@ -0,0 +1,27 @@
+package Git;
+
+public class task {
+    int i;
+    String str;
+
+    public task(int i, String str) {
+        this.i = i;
+        this.str = str;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+}
